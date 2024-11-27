be6225114af000f16527e4e12bd0dd53036861e4
Suraksha-Shetty29 <surakshasuraksha99@gmail.com>
2024-11-27 13:24:41 +0530

    phone number,format

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -9,26 +9,26 @@ class AuthController extends Controller
 {
     //
     public function login(Request $request)
-{
+    {
 
-    $credentials = $request->only('user_name', 'password');
+        $credentials = $request->only('user_name', 'password');
 
-    if (!Auth::attempt(['user_name' => $credentials['user_name'], 'password' => $credentials['password']])) {
-        return response()->json(['error' => 'Incorrect username or password'], 401);
+        if (!Auth::attempt(['user_name' => $credentials['user_name'], 'password' => $credentials['password']])) {
+            return response()->json(['error' => 'Incorrect username or password'], 401);
+        }
+
+        $user = Auth::user();
+        $user->load('roles');
+        $token = $user->createToken('auth_token')->plainTextToken;
+        // dd($token);
+
+        return response()->json([
+            'message' => 'Login successful',
+            'token' => $token,
+            'token_type' => 'Bearer',
+            'user' => $user,
+        ]);
     }
-
-    $user = Auth::user();
-    $user->load('roles');
-    $token = $user->createToken('auth_token')->plainTextToken;
-    // dd($token);
-
-    return response()->json([
-        'message' => 'Login successful',
-        'token' => $token,
-        'token_type' => 'Bearer',
-        'user' => $user,
-    ]);
-}
 
     public function logout(Request $request)
     {
