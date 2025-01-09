aebd8cf8e774eaa368c6d58473d6701903d29b6d
Jithesh866 <jithesh.s1999@gmail.com>
2025-01-09 16:25:58 +0530

    global month selection column added

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -1,5 +1,8 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+
+
+
+import { onMounted, ref, computed } from 'vue'
 import { useColorModes } from '@coreui/vue'
 
 import AppBreadcrumb from '@/components/AppBreadcrumb.vue'
@@ -9,6 +12,33 @@ import { useSidebarStore } from '@/stores/sidebar.js'
 const headerClassNames = ref('mb-4 p-0')
 const { colorMode, setColorMode } = useColorModes('coreui-free-vue-admin-template-theme')
 const sidebar = useSidebarStore()
+const month = ref({
+  month: new Date().getMonth(),
+  year: new Date().getFullYear()
+});
+
+const selectedMonthYear = ref(`${new Date().toLocaleString('default', { month: 'short' }).toUpperCase()}/${new Date().getFullYear()}`) // Default to current month/year
+
+// Generate Month/Year Options
+const months = [
+  'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN',
+  'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC',
+]
+const years = Array.from({ length: 10 }, (_, i) => new Date().getFullYear() - i) // Generate last 10 years
+const monthYearOptions = computed(() => {
+  const options = []
+  years.forEach((year) => {
+    months.forEach((month) => {
+      options.push(`${month}/${year}`)
+    })
+  })
+  return options
+})
+
+const handleMonthYearChange = () => {
+  console.log('Selected Month/Year:', selectedMonthYear.value)
+  // Use this value globally
+}
 
 onMounted(() => {
   document.addEventListener('scroll', () => {
@@ -29,12 +59,20 @@ onMounted(() => {
       </CHeaderToggler>
       <CHeaderNav class="d-none d-md-flex">
         <CNavItem>
-          <CNavLink href="/dashboard"> Dashboard </CNavLink>
+          <CNavLink href="/dashboard"> S Finance </CNavLink>
         </CNavItem>
       
       </CHeaderNav>
       <CHeaderNav class="ms-auto">
-        
+        <!-- Combined Month/Year Selector -->
+        <!-- <div class="d-flex align-items-center me-4">
+          <CFormSelect v-model="selectedMonthYear"  @change="handleMonthYearChange" class="w-auto">
+            <option v-for="option in monthYearOptions" :key="option" :value="option">
+              {{ option }}
+            </option>
+          </CFormSelect>
+        </div> -->
+        <VueDatePicker v-model="month" month-picker />
       </CHeaderNav>
       <CHeaderNav>
         <li class="nav-item py-1">
